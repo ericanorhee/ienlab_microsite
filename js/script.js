72e654443de243fd1bad0ea44899e20adab7ed99
Ericano Rhee <ericano.rhee@gmail.com>
2020-08-31 05:32:17 +0900

    200831_ienlab_microsite
    페이지 이동 스무스하게 (슬라이드 형식)
    휴대폰 스크린샷, 틀 및 문구, 아이콘 추가.

--- js/script.js
+++ js/script.js
@@ -1,54 +1,82 @@
 // scrollDisable();
+// window.onscroll = function() { scrollFunction() };
 
-window.onscroll = function() { scrollFunction() };
+$(function () {
+    $('#fullpage').fullpage({
+        autoScrolling: true,
 
+        scrollHorizontally: true,
+        keyboardScrolling: true,
+        anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+        animateAnchor: true,
+        onLeave: function (origin, destination, direction) {
+            const ic_ienlab = $("#ic_ienlab");
+            const ic_bp = $("#ic_bp");
+            const ic_tn = $("#ic_tn");
+            const ic_ir = $("#ic_ir");
+            const ic_ar = $("#ic_ar");
+            const ic_sg = $("#ic_sg");
 
+            const phone1 = $(".phone.one")
+            const phone2 = $(".phone.two")
 
-function scrollFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    
-    // console.log(scrolled);
+            console.log(destination.index);
+            switch (destination.index) {
+                case 0:
+                    ic_ienlab.addClass('indicator');
+                    ic_bp.removeClass('indicator');
+                    ic_tn.removeClass('indicator');
+                    ic_ir.removeClass('indicator');
+                    ic_ar.removeClass('indicator');
+                    ic_sg.removeClass('indicator');
+                    phone1.addClass('disappear')
+                    phone2.addClass('disappear')
+                    break;
+                case 1:
+                    ic_ienlab.removeClass('indicator');
+                    ic_bp.addClass('indicator');
+                    ic_tn.removeClass('indicator');
+                    ic_ir.removeClass('indicator');
+                    ic_ar.removeClass('indicator');
+                    ic_sg.removeClass('indicator');
+                    phone1.removeClass('disappear')
+                    phone2.removeClass('disappear')
+                    break;
+                case 2:
+                    ic_ienlab.removeClass('indicator');
+                    ic_bp.removeClass('indicator');
+                    ic_tn.addClass('indicator');
+                    ic_ir.removeClass('indicator');
+                    ic_ar.removeClass('indicator');
+                    ic_sg.removeClass('indicator');
+                    break;
+                case 3:
+                    ic_ienlab.removeClass('indicator');
+                    ic_bp.removeClass('indicator');
+                    ic_tn.removeClass('indicator');
+                    ic_ir.addClass('indicator');
+                    ic_ar.removeClass('indicator');
+                    ic_sg.removeClass('indicator');
+                    break;
+                case 4:
+                    ic_ienlab.removeClass('indicator');
+                    ic_bp.removeClass('indicator');
+                    ic_tn.removeClass('indicator');
+                    ic_ir.removeClass('indicator');
+                    ic_ar.addClass('indicator');
+                    ic_sg.removeClass('indicator');
+                    break;
 
-    if (scrolled <= 24.95) {
-        $("#ic_ienlab").addClass('indicator');
-        $("#ic_bp").removeClass('indicator');
-        $("#ic_tn").removeClass('indicator');
-        $("#ic_ir").removeClass('indicator');
-        $("#ic_ar").removeClass('indicator');
-    } else if (scrolled <= 49.95) {
-        $("#ic_bp").addClass('indicator');
-        $("#ic_ienlab").removeClass('indicator');
-        $("#ic_tn").removeClass('indicator');
-        $("#ic_ir").removeClass('indicator');
-        $("#ic_ar").removeClass('indicator');
-    } else if (scrolled <= 74.95) {
-        $("#ic_tn").addClass('indicator');
-        $("#ic_ienlab").removeClass('indicator');
-        $("#ic_bp").removeClass('indicator');
-        $("#ic_ir").removeClass('indicator');
-        $("#ic_ar").removeClass('indicator');
-    } else if (scrolled <= 99.95) {
-        $("#ic_ir").addClass('indicator');
-        $("#ic_ienlab").removeClass('indicator');
-        $("#ic_bp").removeClass('indicator');
-        $("#ic_tn").removeClass('indicator');
-        $("#ic_ar").removeClass('indicator');
-    } else {
-        $("#ic_ar").addClass('indicator');
-        $("#ic_ienlab").removeClass('indicator');
-        $("#ic_bp").removeClass('indicator');
-        $("#ic_tn").removeClass('indicator');
-        $("#ic_ir").removeClass('indicator');
-    }
+                case 5:
+                    ic_ienlab.removeClass('indicator');
+                    ic_bp.removeClass('indicator');
+                    ic_tn.removeClass('indicator');
+                    ic_ir.removeClass('indicator');
+                    ic_ar.removeClass('indicator');
+                    ic_sg.addClass('indicator');
+                    break;
 
-}
-
-function scrollDisable() {
-    $('html, body').addClass('hidden');
-}
-
-function scrollAble() {
-    $('html, body').removeClass('hidden');
-}
+            }
+        }
+    });
+});
