b344bec9889081e2f219daf4f523cfb320e2fb08
Ericano Rhee <ericano.rhee@gmail.com>
2020-09-02 23:33:21 +0900

    200902_ienlab_microsite
    새 페이지 (블로그 플래너) 생성 및 수정 중

---
+++ js/script-blogplanner.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+
+        scrollHorizontally: true,
+        keyboardScrolling: true,
+        animateAnchor: true,
+    });
+});
